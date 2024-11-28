a1c3731606ffae46464a25889d028e7f803d6832
liuyuchen777 <liuyuchen.hust@gmail.com>
2024-11-28 20:14:27 +0900

    Add netty deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  adapter: netlify(),
 });
